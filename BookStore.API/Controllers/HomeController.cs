89c757d1672f8fcacd63ecd65409a2cebd112d64
Jon Cannon <cannonjw@gmail.com>
2020-07-20 08:28:48 -0600

    First Commit

---
+++ BookStore.API/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.API.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _logger.LogInfo("Hello, World!");
+            return Ok("Hello, World!");
+        }
+    }
+}
